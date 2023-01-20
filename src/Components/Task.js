8c336b05a8e01b2a141f40cb79acc5cabac50294
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-01-20 17:19:10 -0500

    TaskList component done

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -2,13 +2,13 @@ import React from "react";
 import "../Stylesheets/Task.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Task({ text, completed }){
+function Task({ id, text, completed, completeTask, deleteTask }){
     return(
         <div className={completed ? "task-container completed" : "task-container"}>
-            <div className="task-text">
+            <div className="task-text" onClick={() => completeTask(id)}>
                 {text}
             </div>
-            <div className="task-icon"> 
+            <div className="task-icon" onClick={() => deleteTask(id)}> 
                 <AiOutlineCloseCircle className="task-icon"></AiOutlineCloseCircle>
             </div>
         </div>
--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Stylesheets/TaskForm.css";
+import { v4 as uuidv4 } from "uuid";
 
 function TaskForm(props) {
+
+    const [input, setInput] = useState('');
+
+    const handleInput = e =>{
+        setInput(e.target.value);
+        console.log(input);
+    }
+    const handleSubmit = e => {
+        e.preventDefault();
+        const newTask = {
+            id: uuidv4(),
+            text: input,
+            completed: false
+        }
+
+        props.onSubmit(newTask);
+    }
+
     return(
-        <form className="task-form">
+        <form className="task-form" onSubmit={handleSubmit}>
             <input
                 className="task-input"
                 type="text"
                 placeholder="Write a task."
                 name="text"
+                onChange={handleInput}
             />
             <button className="task-button">Add Task</button>
         </form>
